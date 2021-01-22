e20bad3fb2aba8a7ccfd8f7d12533dfa43961d1a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-22 19:25:43 +0100

    Write access log to file and console

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import rTracer from 'cls-rtracer'
+import fs from 'fs'
+import path from 'path'
 import { createResponse } from './common'
 import { HttpStatus, ResponseType } from './config'
 
@@ -16,11 +18,22 @@ server.use(
     limit: '124kb',
   })
 )
+
 server.use(rTracer.expressMiddleware())
 
 const morganFormat =
   '[:requestId] :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
-server.use(morgan(morganFormat))
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, '../logs/access.log'),
+  { flags: 'a' }
+)
+server.use(morgan(morganFormat)) // to console
+server.use(
+  // to file
+  morgan(morganFormat, {
+    stream: accessLogStream,
+  })
+)
 morgan.token('requestId', (): string => <string>rTracer.id())
 
 server.use(helmet())
