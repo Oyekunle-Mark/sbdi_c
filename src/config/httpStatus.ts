f67115face18fdf6e85d0c764bc674239a255b3d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-22 19:01:56 +0100

    Create response is a utility for generating and sending server responses

--- src/config/httpStatus.ts
+++ src/config/httpStatus.ts
@@ -2,11 +2,18 @@ export interface HttpStatusI {
   [propName: string]: number
 }
 
-export type ResponseType = boolean
+export interface ResponseTypeI {
+  [propName: string]: boolean
+}
 
 export const HttpStatus: HttpStatusI = {
   StatusOk: 200,
   StatusBadRequest: 400,
   StatusUnauthorized: 401,
   StatusInternalServerError: 500,
+}
+
+export const ResponseType: ResponseTypeI = {
+  Success: true,
+  Failure: false,
 }
