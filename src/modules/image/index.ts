d24f26b758949aac4426f26bea572fbccedc4300
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-25 19:51:28 +0100

    Protected invoice endpoints

--- src/modules/image/index.ts
+++ src/modules/image/index.ts
@@ -1,20 +1,23 @@
 import { Router } from 'express'
 import { getOneImage, getUserTrash, findUserImages } from './image.controller'
 import ImageValidator from './image.validation'
+import { verifyToken } from '../../common'
 
 const imageRoute = Router()
 
 imageRoute.get(
   '/:id',
+  verifyToken,
   ImageValidator.getOneImageValidationRules(),
   ImageValidator.validate,
   getOneImage
 )
 
-imageRoute.get('/trash', getUserTrash)
+imageRoute.get('/trash', verifyToken, getUserTrash)
 
 imageRoute.get(
   '/user/:id',
+  verifyToken,
   ImageValidator.getOneImageValidationRules(),
   ImageValidator.validate,
   findUserImages
